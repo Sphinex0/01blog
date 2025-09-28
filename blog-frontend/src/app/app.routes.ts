9b38d794cec2640d4052b5fa2292437ba18fabc8
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-09-28 17:13:50 +0100

    working on design...

--- blog-frontend/src/app/app.routes.ts
+++ blog-frontend/src/app/app.routes.ts
@@ -3,33 +3,33 @@ import { authGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadComponent: () => import('./layout/main-layout/main-layout.component').then(c => c.MainLayoutComponent),
-  //   canActivate: [authGuard],
-  //   children: [
-  //     {
-  //       path: '',
-  //       loadChildren: () => import('./features/home/home.routes').then(r => r.homeRoutes),
-  //     },
-  //     {
-  //       path: 'profile',
-  //       loadChildren: () => import('./features/profile/profile.routes').then(r => r.profileRoutes),
-  //     },
-  //     {
-  //       path: 'posts',
-  //       loadChildren: () => import('./features/posts/posts.routes').then(r => r.postRoutes),
-  //     },
-  //     {
-  //       path: 'notifications',
-  //       loadChildren: () => import('./features/notifications/notifications.routes').then(r => r.notificationRoutes),
-  //     },
-  //     {
-  //       path: 'settings',
-  //       loadChildren: () => import('./features/settings/settings.routes').then(r => r.settingsRoutes),
-  //     }
-  //   ]
-  // },
+  {
+    path: '',
+    loadComponent: () => import('./layout/main-layout/main-layout.component').then(c => c.MainLayoutComponent),
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'feed',
+        loadChildren: () => import('./features/home/home.routes').then(r => r.homeRoutes),
+      },
+      // {
+      //   path: 'profile',
+      //   loadChildren: () => import('./features/profile/profile.routes').then(r => r.profileRoutes),
+      // },
+      // {
+      //   path: 'posts',
+      //   loadChildren: () => import('./features/posts/posts.routes').then(r => r.postRoutes),
+      // },
+      // {
+      //   path: 'notifications',
+      //   loadChildren: () => import('./features/notifications/notifications.routes').then(r => r.notificationRoutes),
+      // },
+      // {
+      //   path: 'settings',
+      //   loadChildren: () => import('./features/settings/settings.routes').then(r => r.settingsRoutes),
+      // }
+    ]
+  },
   // {
   //   path: 'admin',
   //   loadComponent: () => import('./layout/admin-layout/admin-layout.component').then(c => c.AdminLayoutComponent),
