0743ace4b57a7617641512caac2d4330d046ebd7
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-12-02 14:32:47 +0100

    save

--- backend/src/main/java/api/backend/repository/NotificationRepository.java
+++ backend/src/main/java/api/backend/repository/NotificationRepository.java
@@ -8,12 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Page<Notification> findByReceiverIdAndIdLessThan(long userId, long cursor, Pageable pageable);
 
     long countByReceiverIdAndReadFalse(long userId);
 
+    @Transactional
     @Modifying
     @Query("UPDATE Notification n SET n.read = true WHERE n.receiver.id = :userId AND n.read = false")
     void markAllAsRead(@Param("userId") long userId);
